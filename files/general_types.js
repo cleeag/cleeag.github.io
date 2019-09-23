e6b8a7f9de8b1b8aaa1105e943d900d1d5090d89
cleeag <33797239+cleeag@users.noreply.github.com>
2019-09-24 00:58:46 +0800

    Update general_types.js

--- files/general_types.js
+++ files/general_types.js
@@ -1,1 +1,1 @@
-var general_str=["person	人","concept	概念","organization	组织","location	位置","living_thing	生物","time	时间","object	宾语","event	事件","creation	创建"]
+var general_str=["person	人","concept	概念","organization	组织","location	位置","living_thing	生物","time	时间","object	宾语","event	事件","creation	创建"];
